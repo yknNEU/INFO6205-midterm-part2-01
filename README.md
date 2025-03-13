10af6b572fda8efc77d8535217030317157b0908
Gaurav Gunjal <gunjal.ga@northeastern.edu>
2025-03-13 00:20:30 -0700

    readme updated

---
+++ README.md
@@ -0,0 +1,5 @@
+## How to Complete the Assignment
+### Step 1: Fork the repository
+### Step 2: Implement the `sink` and `swim` Method
+### Step 3: Run the Tests
+### Step 4: Commit your code and submit the url for your fork on canvas (make sure your repo is public)
--- src/main/java/MinPQ.java
+++ src/main/java/MinPQ.java
@@ -4,29 +4,14 @@ import java.util.NoSuchElementException;
 
 /**
  *  The MinPQ class represents a priority queue of generic keys.
- *  It supports the usual <em>insert</em> and <em>delete-the-minimum</em>
+ *  It supports the usual insert and delete-the-minimum
  *  operations, along with methods for peeking at the minimum key,
  *  testing if the priority queue is empty, and iterating through
  *  the keys.
- *  <p>
- *  This implementation uses a <em>binary heap</em>.
- *  The <em>insert</em> and <em>delete-the-minimum</em> operations take
- *  &Theta;(log <em>n</em>) amortized time, where <em>n</em> is the number
- *  of elements in the priority queue. This is an amortized bound
- *  (and not a worst-case bound) because of array resizing operations.
- *  The <em>min</em>, <em>size</em>, and <em>is-empty</em> operations take
- *  &Theta;(1) time in the worst case.
- *  Construction takes time proportional to the specified capacity or the
- *  number of items used to initialize the data structure.
- *  <p>
- *  For additional documentation, see
- *  <a href="https://algs4.cs.princeton.edu/24pq">Section 2.4</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *
- *  @param <Key> the generic type of key on this priority queue
+ *  
+ *  you have to complete the swim and sink methods
+ *  The insert and delMin methods should work correctly after you complete the swim and sink methods
+ *  You can use the MinPQTest class to test your implementation
  */
 public class MinPQ<Key> implements Iterable<Key> {
     private Key[] pq;                    // store items at indices 1 to n
@@ -249,9 +234,4 @@ public class MinPQ<Key> implements Iterable<Key> {
         }
     }
 
-    /**
-     * Unit tests the {@code MinPQ} data type.
-     *
-     * @param args the command-line arguments
-     */
 }
